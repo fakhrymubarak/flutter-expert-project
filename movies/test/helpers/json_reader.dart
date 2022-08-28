5baf1be72e23379914c0e1011833e0e8bb54fc3b
Fakhry Mubarak <fakhrymubarak@gmail.com>
2022-08-28 16:32:23 +0800

    fix: json_reader for testing purpose

--- movies/test/helpers/json_reader.dart
+++ movies/test/helpers/json_reader.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  if (dir.endsWith('\\movies')) {
+    dir = dir.replaceAll('\\movies', '');
+  } else if (dir.endsWith('/movies')) {
+    dir = dir.replaceAll('/movies', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/movies/test/$name').readAsStringSync();
 }
--- tv_shows/test/helpers/json_reader.dart
+++ tv_shows/test/helpers/json_reader.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  if (dir.endsWith('\\tv_shows')) {
+    dir = dir.replaceAll('\\tv_shows', '');
+  } else if (dir.endsWith('/tv_shows')) {
+    dir = dir.replaceAll('/tv_shows', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/tv_shows/test/$name').readAsStringSync();
 }
